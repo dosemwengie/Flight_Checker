e2007fcb75eef7c7f0e5740a1bef91563a5e85ad
rocproducer1 <thewiz1906@gmail.com>
2017-03-11 17:26:35 +0000

    Fixed syntax error, removed if statement for get_database, if db exists, it will retrieve it, else it will create it.

--- app/scripts/db_init.py
+++ app/scripts/db_init.py
@@ -2,13 +2,19 @@ from pymongo import MongoClient
 from optparse import OptionParser
 
 def main():
+	#Accept host/port parsed command, else use default values
 	parser=OptionParser()
-	parser.add_option("--host",dest="host",default="localhost"))
+	parser.add_option("--host",dest="host",default="localhost")
 	parser.add_option("-p","--port",dest="port",default=27017)
 	(options,args)=parser.parse_args()
+	
+	#Create db connection
 	client=MongoClient(host=options.host,port=options.port)
-	if 'flight_tracker' not in db.database_names():
-		db=client.get_database('flight_tracker')
+	
+	#Retrieve db,else it will be created
+	db=client.get_database('flight_tracker')
+
+	#create collections if they do not exist
 	col_names=db.collection_names()
 	if 'criteria' not in col_names:
 		db.create_collection('criteria')
